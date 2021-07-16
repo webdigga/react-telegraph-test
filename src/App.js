e70376e3093e1d3d3ec759544ce0a7688b87c972
David White <webdigga42@gmail.com>
2021-07-16 20:53:59 +0100

    Render 3 components from App JS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import Header from './Header';
+import SortBar from './SortBar';
+import CommentsList from './CommentsList';
 import './App.css';
 import React from 'react';
 
 class App extends React.Component {
 	render () {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={ logo } className="App-logo" alt="logo" />
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
-				</header>
+			<div>
+				<Header />
+				<SortBar />
+				<CommentsList />
 			</div>
 		);
 	}
