2166c0783e996d662390c903af929119beb41c33
David White <webdigga42@gmail.com>
2021-07-19 20:33:08 +0100

    Adding sort button

--- src/SortBar.js
+++ src/SortBar.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
 class SortBar extends React.Component {
+	sortComments () {
+		// this.content.sort(( a, b ) => {
+		// 	return this.sortButton.dataset.sort === 'asc' ? a.likes - b.likes : b.likes - a.likes;
+		// });
+	}
+
 	render () {
-		return <div>Sortbar</div>;
+		return (
+			<button onClick={ this.sortComments }>
+				Sort
+			</button>
+		);
 	}
 }
 
