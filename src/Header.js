71d9e9e5678f4219ca93f9e605879e8628a27551
David White <webdigga42@gmail.com>
2021-07-27 10:28:52 +0100

    Make header functional component

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,11 @@
 import logo from './logo.svg';
-import React from 'react';
 
-class Header extends React.Component {
-	render () {
-		return (
-			<div className="posts__menu">
-				<img src={ logo } className="site-logo" alt="logo" />
-			</div>
-		);
-	}
+function Header() {
+	return (
+		<div className="posts__menu">
+			<img src={ logo } className="site-logo" alt="logo" />
+		</div>
+	);
 }
 
 export default Header;
